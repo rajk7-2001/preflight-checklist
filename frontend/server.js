c28c34bde627052fca50a7e00a30798981050ba7
Raj123454321 <rajkesavan10122001@gmail.com>
2025-11-16 21:25:34 +0530

    updated code

--- frontend/server.js
+++ frontend/server.js
@@ -4,14 +4,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'dist')));
+const distPath = path.join(__dirname, 'dist');
 
-// For React Router or Vue Router client-side routing
+app.use(express.static(distPath));
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(distPath, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,34 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  entry: './src/index.js',
-  output: { path: path.resolve(__dirname, 'dist'), filename: 'bundle.js', publicPath:'/' },
-  module: { rules: [{ test:/\.js$/, exclude:/node_modules/, use:{ loader:'babel-loader', options:{ presets:['@babel/preset-react'] } } }] },
-  devServer:{ static:'./dist', port:3000, hot:true },
-  mode:'development'
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+    clean: true, // cleans dist folder before build
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // REQUIRED!
+      filename: "index.html",
+    }),
+  ],
 };
